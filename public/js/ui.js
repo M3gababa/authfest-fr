3e60fc762e519a33853eee26286e670890378e8b
Glitch (authrocks-cic-ui-template) <none>
2022-09-07 20:37:36 +0000

    🏸📟 Updated with Glitch

---
+++ public/js/ui.js
@@ -0,0 +1,134 @@
+// URL mapping, from hash to a function that responds to that URL action
+const router = {
+  "/": () => showContent("content-home"),
+  "/profile": () =>
+    requireAuth(() => showContent("content-profile"), "/profile"),
+  "/login": () => login(),
+};
+
+//Declare helper functions
+
+/**
+ * Iterates over the elements matching 'selector' and passes them
+ * to 'fn'
+ * @param {*} selector The CSS selector to find
+ * @param {*} fn The function to execute for every element
+ */
+const eachElement = (selector, fn) => {
+  for (let e of document.querySelectorAll(selector)) {
+    fn(e);
+  }
+};
+
+/**
+ * Tries to display a content panel that is referenced
+ * by the specified route URL. These are matched using the
+ * router, defined above.
+ * @param {*} url The route URL
+ */
+const showContentFromUrl = (url) => {
+  if (router[url]) {
+    router[url]();
+    return true;
+  }
+
+  return false;
+};
+
+/**
+ * Returns true if `element` is a hyperlink that can be considered a link to another SPA route
+ * @param {*} element The element to check
+ */
+const isRouteLink = (element) =>
+  element.tagName === "A" && element.classList.contains("route-link");
+
+/**
+ * Displays a content panel specified by the given element id.
+ * All the panels that participate in this flow should have the 'page' class applied,
+ * so that it can be correctly hidden before the requested content is shown.
+ * @param {*} id The id of the content to show
+ */
+const showContent = (id) => {
+  eachElement(".page", (p) => p.classList.add("hidden"));
+  document.getElementById(id).classList.remove("hidden");
+};
+
+/**
+ * Updates the user interface
+ */
+const updateUI = async () => {
+  try {
+    const isAuthenticated = await auth0.isAuthenticated();
+
+    if (isAuthenticated) {
+      const access = await auth0.getTokenSilently();
+
+      const accessJWT = parseJwt(access);
+      
+      if (accessJWT) {
+        document.getElementById("ipt-access-token").innerText = JSON.stringify(
+          accessJWT,
+          null,
+          2
+        );
+      } else {
+        document.getElementById("ipt-access-token").innerText = access;
+      }
+
+      const user = await auth0.getUser();
+
+      document.getElementById("ipt-user-profile").innerText = JSON.stringify(
+        user,
+        null,
+        2
+      );
+
+      document.getElementById("profile-data").innerText = JSON.stringify(
+        user,
+        null,
+        2
+      );
+
+      document.querySelectorAll("pre code").forEach(hljs.highlightBlock);
+
+      eachElement(".profile-image", (e) => (e.src = user.picture));
+      eachElement(".user-name", (e) => (e.innerText = user.name));
+      eachElement(".user-email", (e) => (e.innerText = user.email));
+      eachElement(".auth-invisible", (e) => e.classList.add("hidden"));
+      eachElement(".auth-visible", (e) => e.classList.remove("hidden"));
+    } else {
+      eachElement(".auth-invisible", (e) => e.classList.remove("hidden"));
+      eachElement(".auth-visible", (e) => e.classList.add("hidden"));
+    }
+  } catch (err) {
+    console.log("Error updating UI!", err);
+    return;
+  }
+
+  console.log("UI updated");
+};
+
+window.onpopstate = (e) => {
+  if (e.state && e.state.url && router[e.state.url]) {
+    showContentFromUrl(e.state.url);
+  }
+};
+
+function parseJwt(token) {
+  var base64Url = token.split(".")[1];
+  var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  if (base64 == "") {
+    return false;
+  }
+  var jsonPayload = decodeURIComponent(
+    window
+      .atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
+
+  return JSON.parse(jsonPayload);
+}
